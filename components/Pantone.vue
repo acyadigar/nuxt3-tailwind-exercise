b6e1875878bc3f66262f6422bce246a9f2ef5000
acyadigar <alpercanyadigar@gmail.com>
2022-06-20 19:48:46 +0300

    Add pantone components

---
+++ components/Pantone.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grid md:grid-cols-6 gap-6 py-6 text-center px-12">
+    <div v-for="pantone in data" class="border-2 rounded-lg">
+      <div class="h-20 rounded-tl-lg rounded-tr-lg" :style="'background-color:'+pantone.color">
+      </div>
+      <p class="font-medium text-lg my-1 px-2">PANTONE</p>
+      <p class="uppercase my-1 px-2">{{pantone.name}}</p>
+      <p class="text-sm my-1 px-2">{{pantone.pantone_value}}</p>
+      <p class="text-sm my-1 px-2">{{pantone.year}}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data } = await request('GET', 'api/unknown')
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="main">
     <div class="mt-6">
+      <h1 class="text-4xl font-medium my-6 text-center">Users</h1>
       <Table />
+
+      <h1 class="text-4xl font-medium mt-12 text-center">Pantones</h1>
+      <Pantone />
     </div>
   </div>
 </template>
