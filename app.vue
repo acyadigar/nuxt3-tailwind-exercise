211f2bb75ba3aa049bae25cda3bab6f1f44db744
acyadigar <alpercanyadigar@gmail.com>
2022-06-20 17:42:44 +0300

    Add navigation bar

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
+    <nav class="w-full py-4 bg-gray-500 text-neutral-100">
+      <NuxtLink to="/" class="ml-5 font-medium n-link-base">
+        Home
+      </NuxtLink>
+      <NuxtLink v-if="!user" to="/login" class="ml-5 font-medium n-link-base">
+        Login
+      </NuxtLink>
+      <span v-else class="ml-5 n-link-base">{{user.email}}</span>
+    </nav>
     <NuxtPage />
   </div>
 </template>
+
+<script setup>
+const user = useState('user')
+</script>
