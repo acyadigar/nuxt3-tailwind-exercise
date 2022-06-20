f8058214c0ec213f71a1c6ad5e43da826e1bc943
acyadigar <alpercanyadigar@gmail.com>
2022-06-20 17:50:45 +0300

    Add a sidebar which shows valid accounts for mockapi

---
+++ components/ApiAccounts.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="absolute left-0 top-20 bg-gray-200 p-4">
+    <p class="font-medium">Valid users</p>
+    <div v-for="user in users" class="my-2">
+      <p>{{user.email}}</p>
+      <p>pw: cityslicka</p>
+    </div>
+  </div>  
+</template>
+
+<script setup>
+const result = await request('GET', 'api/users')
+const users = computed(() => result.data)
+</script>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="mt-16 flex justify-center items-center">
+    <ApiAccounts />
     <LoginForm />
   </div>
 </template>
